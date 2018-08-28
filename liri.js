8bbf617aaa0466d9230ef760ad776240aeceb5b6
JAMIE DEHAAN <jamiedehaan@JAMIEs-MacBook-Pro.local>
2018-08-28 17:28:00 -0600

    ready to submit

--- liri.js
+++ liri.js
@@ -16,41 +16,35 @@ for (var i = 3; i < arguments.length; i++) {
     userInput.push(arguments[i])
 }
 
-// function concertLife() {
-//     var concert = "";
-//     for (var c = 0; c < userInput.length; c++) {
-//         concert = concert + "+" + userInput[c];
-//     }
-//     if (concert === "") {
-//         concertName = "Ed Sheeran";
-//     }
-//     console.log(concert);
-//     request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function (error, response, body) {
-//         var concertObject = JSON.parse(body);
-
-//         // EDIT CONSOLE.LOGS BELOW!!!
-//         console.log(); Name of the venue
-//         console.log(); Venue location
-//         console.log(); Date of the event (use moment to format this as "MM/DD/YYYY")
-
-//     });
-// };
+function concertLife() {
+    
+    var concert = userInput.join("+");
+    if (concert === "") {
+        concert = "Ed Sheeran";
+    }
+    console.log(concert);
+    request("https://rest.bandsintown.com/artists/" + concert + "/events?app_id=codingbootcamp", function (error, response, body) {
+        var concertObject = JSON.parse(body);
+        for (var j = 0; j < concertObject.length; j++) {
+            console.log("\nVenue Name: " + concertObject[j].venue.name);
+            console.log("Venue Location: " + concertObject[j].venue.city + ", " + concertObject[j].venue.region);
+            console.log("Date Of The Event: " + moment(concertObject[j].datetime).format("MM/DD/YY") + "\r\n");
+        }
+    });
+};
 
 function spotifyLife() {
 
-    var song = "";
-    for (var s = 0; s < userInput.length; s++) {
-        song = song + "+" + userInput[s];
-    }
+    var song = userInput.join("+");
     if (song === "") {
-        songName = "The Sign";
+        song = "The Sign";
     }
-    console.log(song);
+    console.log("song: " + song);
     spotify.search({ type: "track", query: song, limit: 1 }, function (error, data) {
         if (error) {
             return console.log("Error: " + error);
         }
-        console.log("Artist: " + data.tracks.items[0].artists[0].name + "\r\n");
+        console.log("\nArtist: " + data.tracks.items[0].artists[0].name + "\r\n");
         console.log("Song Name: " + data.tracks.items[0].name + "\r\n");
         console.log("Preview Link: " + data.tracks.items[0].preview_url + "\r\n");
         console.log("Album Name: " + data.tracks.items[0].album.name + "\r\n");
@@ -59,17 +53,14 @@ function spotifyLife() {
 
 function movieLife() {
 
-    var movie = "";
-    for (var m = 0; m < userInput.length; m++) {
-        movie = movie + "+" + userInput[m];
-    }
+    var movie = userInput.join("+");
     if (movie === "") {
-        movieName = "Mr Nobody";
+        movie = "Mr. Nobody";
     }
     console.log(movie);
     request("http://www.omdbapi.com/?t=" + movie + "&plot=short&apikey=trilogy", function (error, response, body) {
         var movieObject = JSON.parse(body);
-        console.log("Title: " + movieObject.Title + "\r\n");
+        console.log("\nTitle: " + movieObject.Title + "\r\n");
         console.log("Year: " + movieObject.Year + "\r\n");
         console.log("IMDB Rating: " + movieObject.imdbRating + "\r\n");
         console.log("Rotten Tomatoes Rating: " + movieObject.tomatoRating + "\r\n");
@@ -102,7 +93,7 @@ var userData = function (userInput) {
             break;
 
         case "spotify-this-song":
-            spotifyLife(userInput);
+            spotifyLife();
             break;
 
         case "movie-this":
